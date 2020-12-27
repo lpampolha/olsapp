0bfd95f924c08eab9af74bd143378b83a14f95d0
Lucas Pampolha <lucaspampolhadasilva@gmail.com>
2020-12-27 18:39:27 -0300

    Header com Botões Login, Cadastrar e Post de Anúncio

--- src/components/partials/Header/Header.js
+++ src/components/partials/Header/Header.js
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom'
 import { HeaderStyled } from './styled'
 
 const Header = () => {
     return (
         <HeaderStyled>
-            ...
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">O</span>
+                        <span className="logo-2">L</span>
+                        <span className="logo-3">S</span>
+                    </Link>
+                </div>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="">Cadastrar</Link>
+                        </li>
+                        <li>
+                            <Link to="" className="button">Anuncie Aqui!</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         </HeaderStyled>
     )
 }
