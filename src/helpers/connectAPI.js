9c578ec02d6675b3f58633f2ae4610d494a7b635
Lucas Pampolha <lucaspampolhadasilva@gmail.com>
2020-12-28 09:56:39 -0300

    Remoção de mensagem de teste signin

--- src/helpers/connectAPI.js
+++ src/helpers/connectAPI.js
@@ -1,8 +1,14 @@
 const connectAPI = {
     login: async(email,password) => {
-        return{
-            error: 'Funcionalidade Incompleta'
-        }
+
+        const json = await ClientHTTP(
+            '/user/signin', {email, password}
+        )
+
+        //Teste mostrando erro quando informações no signin não eram inseridas - Pré-API
+        // return{
+        //     error: 'Funcionalidade Incompleta'
+        // }
     }
 }
 
