6588ab38f78ef54a6fb596b13b9a54ffe9fd6d20
Lucas Pampolha <lucaspampolhadasilva@gmail.com>
2021-01-20 10:16:46 -0300

    AdPage

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-//import { Slide } from 'react-slideshow-image';
-import { PageArea, Fake } from './styled';
+import { useParams, Link } from 'react-router-dom';
+import { Slide } from 'react-slideshow-image';
+import { PageArea, Fake, OthersArea, BreadChumb } from './styled';
 import useApi from '../../helpers/ConnectAPI';
 
 import { PageContainer } from '../../components/MainComponents';
-//import AdItem from '../../components/partials/AdItem';
+import AdItem from '../../components/partials/AdItem'
 
 const Page = () => {
     const api = useApi();
@@ -36,12 +36,32 @@ const Page = () => {
 
     return (
         <PageContainer>
-
+            {adInfo.category &&
+            <BreadChumb>
+            
+                    Você está aqui:
+                    <Link to="/">Home</Link>
+                    /
+                    <Link to={`/ads?state=${adInfo.stateName}`}>{adInfo.stateName}</Link>
+                    /
+                    <Link to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}>{adInfo.category.name}</Link>
+                    / {adInfo.title}
+                </BreadChumb>
+            }
             <PageArea>
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
                             {loading && <Fake height={300}/>}
+                            {adInfo.images &&
+                                <Slide>
+                                    {adInfo.images.map((img, k) =>
+                                        <div key={k} className="each-slide">
+                                            <img src={img} alt="" />
+                                        </div>
+                                    )}
+                                </Slide>
+                            }
                         </div>
                         <div className="adInfo">
                             <div className="adName">
@@ -71,50 +91,6 @@ const Page = () => {
                 <div className="rightSide">
                     <div className="box box--padding">
                         {loading && <Fake height={20}/>}
-                    </div>
-                    <div className="box box--padding">
-                        {loading && <Fake height={50}/>}
-                    </div>
-                </div>
-                
-                {/* <div className="leftSide">
-                    <div className="box">
-                        <div className="adImage">
-                            {loading && <Fake height={300} />}
-                            {adInfo.images &&
-                                <Slide>
-                                    {adInfo.images.map((img, k) =>
-                                        <div key={k} className="each-slide">
-                                            <img src={img} alt="" />
-                                        </div>
-                                    )}
-                                </Slide>
-                            }
-                        </div>
-                        <div className="adInfo">
-                            <div className="adName">
-                                {loading && <Fake height={20} />}
-                                {adInfo.title &&
-                                    <h2>{adInfo.title}</h2>
-                                }
-                                {adInfo.dateCreated &&
-                                    <small>Criado em {formatDate(adInfo.dateCreated)}</small>
-                                }
-                            </div>
-                            <div className="adDescription">
-                                {loading && <Fake height={100} />}
-                                {adInfo.description}
-                                <hr/>
-                                {adInfo.views &&
-                                    <small>Visualizações: {adInfo.views}</small>
-                                }
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="rightSide">
-                    <div className="box box--padding">
-                        {loading && <Fake height={20} />}
                         {adInfo.priceNegotiable &&
                             "Preço Negociável"
                         }
@@ -122,8 +98,9 @@ const Page = () => {
                             <div className="price">Preço: <span>R$ {adInfo.price}</span></div>
                         }
                     </div>
-                    {loading && <Fake height={50} />}
-                    {adInfo.userInfo &&
+                    <div className="box box--padding">
+                        {loading && <Fake height={50}/>}
+                        {adInfo.userInfo &&
                         <>
                             <a href={`mailto:${adInfo.userInfo.email}`} target="_blank" className="contactSellerLink">Fale com o vendedor</a>
                             <div className="createdBy box box--padding">
@@ -133,8 +110,22 @@ const Page = () => {
                             </div>
                         </>
                     }
-                </div> */}
+                    </div>
+                </div>
+                    
             </PageArea>
+                <OthersArea>
+                    {adInfo.others &&
+                            <>
+                                <h2>Outras ofertas do vendedor</h2>
+                                <div className="list">
+                                    {adInfo.others.map((i,k)=>
+                                        <AdItem key={k} data={i} />
+                                    )}
+                                </div>
+                            </>
+                        }
+                </OthersArea>
         </PageContainer>
     );
 }
