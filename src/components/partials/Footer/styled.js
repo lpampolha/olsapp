1ac0778656f2107a91e7af5f503a2ffdfa95e600
Lucas Pampolha <lucaspampolhadasilva@gmail.com>
2021-01-20 22:38:14 -0300

    Rodapé estlizado

--- src/components/partials/Footer/styled.js
+++ src/components/partials/Footer/styled.js
@@ -2,6 +2,12 @@ import styled from 'styled-components'
 
 export const FooterStyled = styled.div`
     height: 100px;
-    background-color: #ccc;
+    display: flex;
+    justify-content: center;
+    align-itens: center;
     text-align: center;
+    color: #999;
+    border-top: 1px solid #ccc;
+    margin-top: 20px;
+
 `
